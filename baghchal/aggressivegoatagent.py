382a38aa9368309871a10b96ef56e47e1203f684
Grant Kingma <gakingma@mavs.coloradomesa.edu>
2020-03-03 16:16:32 -0700

    aggressive goat agent

--- baghchal/aggressivegoatagent.py
+++ baghchal/aggressivegoatagent.py
@@ -5,17 +5,17 @@ from move import Move
 from typing import List
 import random
 
-class MyScaredGoatAgent(Agent):
+class AggressiveGoatAgent(Agent):
     def __init__(self, game : Game):
-        super(MyScaredGoatAgent, self).__init__(game,Const.MARK_GOAT)
+        super(AggressiveGoatAgent, self).__init__(game,Const.MARK_GOAT)
     def propose(self) -> Move:
         moves = self.game.goatMoves()
-        safeMoves : list[Move]=[]
-        safeMovesPriority : list[int]=[]
+        safeMoves : List[Move]=[]
+        safeMovesPriority : List[int]=[]
         for move in moves:
             self.game.play(move)
             tigerMoves = self.game.tigerMoves()
-            captures : list[Move]=[]
+            captures : List[Move]=[]
             for tMove in tigerMoves:
                 if tMove.capture:
                     captures.append(move)
@@ -23,7 +23,7 @@ class MyScaredGoatAgent(Agent):
                 safeMoves.append(move)
                 safeMovesPriority.append(len(tigerMoves))
             self.game.unplay(move)
-        bestMoves : list[Move]=[]
+        bestMoves : List[Move]=[]
         if len(safeMoves) != 0:
             bestPriority = min(safeMovesPriority)
             for i in range(len(safeMoves)):
--- baghchal/main.py
+++ baghchal/main.py
@@ -1,20 +1,19 @@
-from hungrytigeragent import HungryTigerAgent
 from game import Game
 import random
 from matchup import Matchup
 from hungrytigeragent import HungryTigerAgent
 from scaredgoatagent import ScaredGoatAgent
-from myscaredgoatagent import MyScaredGoatAgent
+from aggressivegoatagent import AggressiveGoatAgent
 from stats import Stats
 
-matchup = Matchup()
+matchup = Matchup()  
 matchup.tigerAgent = HungryTigerAgent(matchup.game)
-matchup.goatAgent = MyScaredGoatAgent(matchup.game)
+matchup.goatAgent = AggressiveGoatAgent(matchup.game)
 
 #while not matchup.over:
     #matchup.turn()
     #print(matchup.game)
 
-stats = Stats(matchup, 1000)
+stats = Stats(matchup, 100)
 stats.playAll()
 stats.summarize()
